9f55f9351908c134a33366c869267690393060ae
Gabri Botha <angelodeath@outlook.com>
2023-05-25 17:36:09 +0200

    Add rest of method bindings to GbeMonsters and GbeBullets

--- src/hx/GbeBullets.hx
+++ src/hx/GbeBullets.hx
@@ -1,6 +1,7 @@
 package hx;
 
 import godot.*;
+import godot.variant.*;
 
 class GbeBullets extends Node2D {
     override function _ready() {
@@ -8,5 +9,36 @@ class GbeBullets extends Node2D {
             return;
 
         trace("GbeBullets._ready()");
+    }
+    @:export
+    override function _process(_delta:Float):Void {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeBullets._process()");
+        // this.queue_redraw();
+    }
+    @:export
+    override function _physics_process(_delta:Float):Void {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeBullets._physics_process()");
+    }
+    @:export
+    override function _draw() {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeBullets._draw()");
+    }
+    @:export
+    public function addBullet(_speed:Float, _direction:Vector2, _texture:Texture, _player_position:Vector2) {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeBullets.addBullet()");
+    }
+    @:export
+    override function _exit_tree(){
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeBullets._exit_tree()");
     }
 }
--- src/hx/GbeMonsters.hx
+++ src/hx/GbeMonsters.hx
@@ -1,6 +1,7 @@
 package hx;
 
 import godot.*;
+import godot.variant.*;
 
 class GbeMonsters extends Node2D {
     override function _ready() {
@@ -8,5 +9,36 @@ class GbeMonsters extends Node2D {
             return;
 
         trace("GbeMonsters._ready()");
+    }
+    @:export
+    override function _process(_delta:Float):Void {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeMonsters._process()");
+        // this.queue_redraw();
+    }
+    @:export
+    override function _physics_process(_delta:Float):Void {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeMonsters._physics_process()");
+    }
+    @:export
+    override function _draw() {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeMonsters._draw()");
+    }
+    @:export
+    public function addMonster(_speed:Float, _direction:Vector2, _texture:Texture, _player_position:Vector2) {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeMonsters.addMonster()");
+    }
+    @:export
+    override function _exit_tree(){
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeMonsters._exit_tree()");
     }
 }
