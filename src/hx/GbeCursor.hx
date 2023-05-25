b3e9d96bb1c3d41a3fbd65906174ff140dfe556f
Gabri Botha <angelodeath@outlook.com>
2023-05-25 17:21:08 +0200

    Build GbeCursor (working)

--- src/hx/GbeCursor.hx
+++ src/hx/GbeCursor.hx
@@ -1,12 +1,17 @@
 package hx;
 
 import godot.*;
+import godot.variant.*;
 
 class GbeCursor extends Node2D {
+    @:export
     override function _ready() {
-        if (Engine.singleton().is_editor_hint()) // skip if in editor
+        if (Engine.singleton().is_editor_hint())
             return;
 
         trace("GbeCursor._ready()");
+        var arrowTexture = ResourceLoader.singleton().load("res://assets/sprites/Reticle.png", "", 1).as(Texture);
+        var hotspot = new Vector2(0, 0);
+        Input.singleton().set_custom_mouse_cursor(arrowTexture, hotspot);
     }
 }
