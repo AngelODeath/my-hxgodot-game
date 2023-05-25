e39424b2e7af350ea129eeae3293fb32ccdebe14
Gabri Botha <angelodeath@outlook.com>
2023-05-25 16:25:21 +0200

    Add setScore and incrementScore methods

--- src/hx/GbeScore.hx
+++ src/hx/GbeScore.hx
@@ -3,10 +3,23 @@ package hx;
 import godot.*;
 
 class GbeScore extends Label {
+    @:export public var score(default,default):Int = 0;
+
+    @:export
     override function _ready() {
         if (Engine.singleton().is_editor_hint()) // skip if in editor
             return;
 
         trace("GbeScore._ready()");
+    }
+    @:export
+    public function setScore(_score:Int) {
+        score = _score;
+        this.set_text('Score: $score');
+    }
+    @:export
+    public function incrementScore() {
+        score++;
+        this.set_text('Score: $score');
     }
 }
