b68412a638232b682fc10be4ea8e2685e35bb563
Gabri Botha <angelodeath@outlook.com>
2023-05-29 16:28:52 +0200

    Commit current working state

--- src/hx/GbeMain.hx
+++ src/hx/GbeMain.hx
@@ -1,22 +1,43 @@
 package hx;
 
 import godot.*;
+import godot.variant.*;
+import hx.*;
 
 class GbeMain extends Node2D {
+    public var player:GbePlayer;
+
     override function _ready() {
         if (Engine.singleton().is_editor_hint()) // skip if in editor
             return;
 
         trace("GbeMain._ready()");
+        player = this.get_node("Player").as(hx.GbePlayer);
+        player.onHit.connect(Callable.fromObjectMethod(this, "onPlayerHit"), 0);
+        player.onFire.connect(Callable.fromObjectMethod(this, "onPlayerFire"), 0);
+        trace('$player signals connected');
     }
 
     @:export
     public function onMobTimer() {
+        if (Engine.singleton().is_editor_hint()) // skip if in editor
+            return;
         trace("GbeMain.onMobTimer()");
     }
+
+    @:export
+    public function onPlayerHit() {
+        if (Engine.singleton().is_editor_hint()) // skip if in editor
+            return;
+        trace("GbeMain.onPlayerHit()");
+        // player.onPlayerFire();
+    }
+    
     @:export
     public function onPlayerFire() {
+        if (Engine.singleton().is_editor_hint()) // skip if in editor
+            return;
         trace("GbeMain.onPlayerFire()");
-        // player.onPlayerFire();
+        player.fireBullet();
     }
 }
--- src/hx/GbePlayer.hx
+++ src/hx/GbePlayer.hx
@@ -1,34 +1,53 @@
 package hx;
 
 import godot.*;
+import godot.variant.*;
 
 class GbePlayer extends CharacterBody2D {
+    public var onHit:TypedSignal<()->Void>;
+    public var onFire:TypedSignal<()->Void>;
+
+    var MoveRight:StringName = "right";
+    var MoveLeft:StringName = "left";
+    var MoveUp:StringName = "up";
+    var MoveDown:StringName = "down";
+
     public function new() {
         super();
         trace("new GbePlayer()");
+        onHit = Signal.fromObjectSignal(this, "onHit");
+        onFire = Signal.fromObjectSignal(this, "onFire");
     }
+
     @:export
     override function _ready() {
-        if (Engine.singleton().is_editor_hint()) // skip if in editor
+        if (Engine.singleton().is_editor_hint())
             return;
         trace("GbePlayer._ready()");
     }
+
     @:export
     override function _physics_process(_delta:Float) {
-        if (Engine.singleton().is_editor_hint()) // skip if in editor
+        if (Engine.singleton().is_editor_hint())
             return;
         trace("GbePlayer._physics_process()");
+
+        // Already normalized
+        var input_vector = Input.singleton().get_vector(MoveLeft, MoveRight, MoveUp, MoveDown);
+        trace(input_vector);
     }
+
     @:export
     function getMouseDirection() {
-        if (Engine.singleton().is_editor_hint()) // skip if in editor
+        if (Engine.singleton().is_editor_hint())
             return;
         trace("GbePlayer.getMouseDirection()");
     }
+
     @:export
-    public function onPlayerFire(){
-        if (Engine.singleton().is_editor_hint()) // skip if in editor
+    public function fireBullet(){
+        if (Engine.singleton().is_editor_hint())
             return;
-        trace("GbePlayer.onPlayerFire()");
+        trace("GbePlayer.fireBullet()");
     }
 }
