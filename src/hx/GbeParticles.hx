980a881aafc1d6867d2fa4ec32a0a6106ff81927
Gabri Botha <angelodeath@outlook.com>
2023-05-30 16:11:47 +0200

    Latest working build

---
+++ src/hx/GbeParticles.hx
@@ -0,0 +1,12 @@
+package hx;
+
+import godot.*;
+
+class GbeParticles extends GPUParticles2D {
+    @:export
+    override function _ready() {
+        if (Engine.singleton().is_editor_hint())
+            return;
+        trace("GbeParticles._ready()");
+    }
+}
--- src/hx/GbePlayer.hx
+++ src/hx/GbePlayer.hx
@@ -11,6 +11,13 @@ class GbePlayer extends CharacterBody2D {
     var MoveLeft:StringName = "left";
     var MoveUp:StringName = "up";
     var MoveDown:StringName = "down";
+    var PrimaryFire:StringName = "primary_fire";
+
+    var speed:Vector2 = new Vector2(600,600);
+    var animationPlayer:AnimatedSprite2D;
+    var fireTimer:Float = -0.5;
+    var fireRate:Float = 0.5;
+
 
     public function new() {
         super();
@@ -24,6 +31,7 @@ class GbePlayer extends CharacterBody2D {
         if (Engine.singleton().is_editor_hint())
             return;
         trace("GbePlayer._ready()");
+        animationPlayer = this.get_node("AnimatedSprite2D").as(AnimatedSprite2D);
     }
 
     @:export
@@ -34,14 +42,48 @@ class GbePlayer extends CharacterBody2D {
 
         // Already normalized
         var input_vector = Input.singleton().get_vector(MoveLeft, MoveRight, MoveUp, MoveDown);
-        trace(input_vector);
+        
+        // Calculate player position based on speed
+        var velocity = speed * input_vector * _delta;
+        var position = this.get_position();
+        position += velocity;
+        this.set_position(position);
+        
+        // Calculate player direction based on mouse cursor
+        var direction = getMouseDirection();
+        var angleRad = Math.atan2(direction.y, direction.x);
+        var angleDeg = angleRad * 180 / Math.PI;
+        this.rotation_degrees = angleDeg;
+
+        var currentTime = cast(Time.singleton().get_ticks_msec(), Float) / 1000.0;
+        if (Input.singleton().is_action_just_pressed(PrimaryFire)) {
+            if (currentTime >= fireTimer+fireRate) {
+                fireTimer = currentTime;
+                this.emit_signal("onFire");
+            }
+            animationPlayer.play("shoot");
+        }
+        // } else if (velocity.length() > 0)
+        //     animationPlayer.play("walk");
+        // else
+        //     animationPlayer.play("base");
+        
+        //Todo: handle collisions
+        // var collision = this.move_and_collide(velocity * _delta);
+        // trace(collision);
+
     }
 
     @:export
-    function getMouseDirection() {
+    function getMouseDirection(): Vector2 {
         if (Engine.singleton().is_editor_hint())
-            return;
+            return new Vector2(0,0);
         trace("GbePlayer.getMouseDirection()");
+        var mousePos = this.get_global_mouse_position();
+        var playerPos = this.global_position;
+        var direction = mousePos - playerPos;
+        direction.normalize();
+        return direction;
     }
 
     @:export
