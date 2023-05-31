d1d71d9b880a249dff0de66e11d436acb86e77d0
Gabri Botha <angelodeath@outlook.com>
2023-05-31 19:42:48 +0200

    Main: Change from Node2D to Node, add escape key handler

--- src/hx/GbeMain.hx
+++ src/hx/GbeMain.hx
@@ -4,7 +4,7 @@ import godot.*;
 import godot.variant.*;
 import hx.*;
 
-class GbeMain extends Node2D {
+class GbeMain extends Node {
     public var player:GbePlayer;
 
     override function _ready() {
@@ -12,11 +12,35 @@ class GbeMain extends Node2D {
             return;
 
         trace("GbeMain._ready()");
-        player = this.get_node("Player").as(hx.GbePlayer);
+        player = get_node("Player").as(hx.GbePlayer);
         player.onHit.connect(Callable.fromObjectMethod(this, "onPlayerHit"), 0);
         player.onFire.connect(Callable.fromObjectMethod(this, "onPlayerFire"), 0);
         trace('$player signals connected');
+        // get_tree().set_auto_accept_quit(true);
     }
+
+    @:export
+    override function _unhandled_input(_event: InputEvent) {
+        // trace('GbeMain._unhandled_input(${_event.as_text()})');
+        if (_event.as_text() == 'Escape') {
+            trace('_event.as_text() == "Escape"');
+            trace('quiting');
+            get_tree().quit();
+        }
+    }
+
+
+    // @:export
+    // override function _notification(_what: Int) {
+    //     trace('GbeMain._notification($_what)');
+    //     // Quit event (Alt+F4,X button)
+    //     if (_what == Node.NOTIFICATION_WM_CLOSE_REQUEST) {
+    //         trace('quiting');
+    //         get_tree().quit(); // default behavior
+    //     }
+    //     // Back event:
+    //     // NOTIFICATION_WM_GO_BACK_REQUEST 
+    // }
 
     @:export
     public function onMobTimer() {
